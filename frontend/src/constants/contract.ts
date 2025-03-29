8c023eacaf32db6037db9028edc76e6b90ce17ee
BB1101 <zhou0346@e.ntu.edu.sg>
2025-03-29 18:23:54 +0800

    Update contract address

--- frontend/src/constants/contract.ts
+++ frontend/src/constants/contract.ts
@@ -4,12 +4,11 @@ import { base } from "thirdweb/chains";
 // import { sepoliaTestnet } from "@/chain.config";
 
 export const bondFactoryAddress = "0xDDdB03448d4dbBd3E6A6B945B0813a5147AE535b";
-export const mockStableCoinAddress = "0x7573492A25674f1359261720A7Bcf6CDDbea04ae";
+export const mockStableCoinAddress = "0x4b84D11FAD4dD6fb6277E055D0892023456eeCFc";
 const sepoliaTestnet = defineChain(11155111);
 
 export const bondContract = getContract({
     client: client,
-    
     // chain: base,
     address: bondFactoryAddress,
     chain: sepoliaTestnet
